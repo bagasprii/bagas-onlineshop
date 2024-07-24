c40c32772a7d3ecb40bc4733b3fb313f86e37dbd
bagasprii <165666301+bagasprii@users.noreply.github.com>
2024-07-24 17:42:23 +0700

    Update contact.blade.php

--- resources/views/pelanggan/page/contact.blade.php
+++ resources/views/pelanggan/page/contact.blade.php
@@ -2,37 +2,20 @@
 
 @section('content')
     <div class="row mt-4 align-items-center">
-        <div class="col-md-6">
-            <div class="content-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti commodi delectus neque, quasi soluta non
-                illum, similique quisquam nostrum iusto magni aspernatur esse. Ipsa cupiditate vitae est iusto labore culpa
-                magnam fugit minima. Voluptate ipsam ad, neque voluptas ducimus perspiciatis praesentium natus iure delectus
-                eaque officia dolores iste asperiores assumenda aliquam dignissimos! Ex odit accusantium ad quaerat, omnis
-                atque
-                minus velit dicta sapiente quam vel, temporibus eligendi! Nobis, molestiae hic praesentium reiciendis
-                exercitationem in debitis quisquam. Totam dolores veritatis reprehenderit et voluptates iure, alias quam
-                consequatur in cumque saepe eius, aperiam repellat temporibus aut? Nobis quam sunt fuga perspiciatis nemo
-                reprehenderit similique tempora dolore, beatae fugiat, obcaecati expedita possimus. Quae fugit tempore,
-                obcaecati excepturi unde laudantium eveniet ratione natus. Distinctio.
-            </div>
-        </div>
-        <div class="col-md-6">
-            <img src="{{ asset('assets/images/office-2.png') }}" style="width:100%" alt="">
-        </div>
     </div>
 
     <div class="d-flex justify-content-lg-between mt-5">
         <div class="d-flex align-items-center gap-4">
             <i class="fa fa-users fa-2x"></i>
-            <p class="m-0 fs-5">+ 300 Pelanggan</p>
+            <p class="m-0 fs-5">+800 Pelanggan</p>
         </div>
         <div class="d-flex align-items-center gap-4">
             <i class="fas fa-home fa-2x"> </i>
-            <p class="m-0 fs-5"> +500 Seller</p>
+            <p class="m-0 fs-5">+500 Seller</p>
         </div>
         <div class="d-flex align-items-center gap-4">
             <i class="fas fa-shirt fa-2x"></i>
-            <p class="m-0 fs-5">+ 300 Product</p>
+            <p class="m-0 fs-5">+100 Product</p>
         </div>
     </div>
 
@@ -48,8 +31,7 @@
                     <h4>Kritik dan saran</h4>
                 </div>
                 <div class="card-body">
-                    <p class="p-0 mb-5 text-lg-center">Masukan kritik dan saran anda kepada aplikasi kami ini agar kami dapat memberikan
-                        apa yang menjadi kebutuhan anda dan kami dapat berkembang lebih baik lagi.
+                    <p class="p-0 mb-5 text-lg-center">Masukan kritik dan saran anda kepada website ini.
                     </p>
                     <div class="mb-3 row">
                         <label for="email" class="col-sm-2 col-form-label">Email</label>
